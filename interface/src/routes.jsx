327400c890b3496f4e46ccde4c6202ef235f0ca9
Lucas Hideki Miyazaki <lucasmiyazaki6@gmail.com>
2024-10-11 11:18:21 -0300

    ......

--- interface/src/routes.jsx
+++ interface/src/routes.jsx
@@ -2,12 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Pessoa from './pages/Pessoa/Pessoa';
 import Login from './pages/Login/Login';
+import ProtectedRoute from './components/Rotas/ProtectedRoutes';
 function AppRouter() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path='/pessoas' element={<Pessoa />} />
+                <Route path='/pessoas' element={<ProtectedRoute element={<Pessoa />} />}/>
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
